e02b21f212dbc238c133e39da11d701c9ff0e62c
Kyle <Kyle.Altemara@gmail.com>
2024-09-05 08:23:41 -0700

    Add xml documentation

--- ExerciseTrackerConsoleApp/Data/IExerciseEntry.cs
+++ ExerciseTrackerConsoleApp/Data/IExerciseEntry.cs
@@ -1,13 +1,44 @@
 ﻿namespace ExerciseTrackerConsoleApp.Data
 {
+    /// <summary>
+    /// Represents an exercise entry.
+    /// </summary>
     public interface IExerciseEntry
     {
-        public string Name { get; }
-        public int Id { get; set; }
-        public DateTime DateStart { get; set; }
-        public DateTime DateEnd { get; }
-        public TimeSpan Duration { get; set; }
-        public string? Comments { get; set; }
-        public string ToString();
+        /// <summary>
+        /// Gets the name of the exercise.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets or sets the ID of the exercise entry.
+        /// </summary>
+        int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start date and time of the exercise.
+        /// </summary>
+        DateTime DateStart { get; set; }
+
+        /// <summary>
+        /// Gets the end date and time of the exercise.
+        /// </summary>
+        DateTime DateEnd { get; }
+
+        /// <summary>
+        /// Gets or sets the duration of the exercise.
+        /// </summary>
+        TimeSpan Duration { get; set; }
+
+        /// <summary>
+        /// Gets or sets the comments for the exercise.
+        /// </summary>
+        string? Comments { get; set; }
+
+        /// <summary>
+        /// Returns a string representation of the exercise entry.
+        /// </summary>
+        /// <returns>A string representation of the exercise entry.</returns>
+        string ToString();
     }
 }
